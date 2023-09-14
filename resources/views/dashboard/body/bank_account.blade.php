21366ffc54eec25a02f510876100e441cf87b17a
AmerDawoood <amermadawood@gmail.com>
2023-09-14 18:06:42 +0300

    update dashboard

--- resources/views/dashboard/body/bank_account.blade.php
+++ resources/views/dashboard/body/bank_account.blade.php
@@ -1,22 +1,33 @@
 <div class="col-lg-7 col-md-6">
     <div class="new-sec">
         <div class="writsec">
-            {{-- <input class="edit" placeholder="اكتب اسم القسم بعد التعديل  ...."> --}}
+            <div class="input-container" style="padding-top:90px;">
+              <input type="text" class="form-control edit" placeholder=" كتابة رقم الحساب" >
+            </div>
+            <div class="button-container">
+                <a href="">
+                    <button class="btn btn-success bt-sd" style="font-size: 20px;">اضافة</button>
 
-            <button class="edit" >اكتب اسم القسم بعد التعديل  ....</button>
-            <button class="btn btn-success bt-sd"><a href="">اضافة</a></button>
-
-        </div>
+                </a>
+            </div>
+          </div>
 
        <div class="ttilesec">
-        <h5>الاقسام المتاحة</h5>
+        <h5>الحسابات المتاحة</h5>
        </div>
 
        <div class="our-sec">
         <div class="col-lg-4 col-md-4">
 
+                <div class="all-buutton2">
+                   <div>
+                    <button type="button" class="btn btn-primary btn-lg btn-add">الحساب</button>
+                    <button type="button" class="btn btn-primary btn-lg btn-add"> تعديل الحساب</button>
 
 
+                    <button type="button" class="btn btn-primary btn-lg bnt-dan">حذف الحساب</button>
+                   </div>
+                </div>
         </div>
        </div>
     </div>
